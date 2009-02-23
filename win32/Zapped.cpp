0280514ccff0afa8f9c02fde275c81ecba4ec15f
Paul Betts <paul@paulbetts.org>
2009-02-22 19:48:26 -0800

    Create scratch window, register our hotkey

--- win32/Zapped.cpp
+++ win32/Zapped.cpp
@@ -7,15 +7,7 @@
 #define MAX_LOADSTRING 100
 
 // Global Variables:
-HINSTANCE hInst;								// current instance
-TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
-TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
-
-// Forward declarations of functions included in this code module:
-ATOM				MyRegisterClass(HINSTANCE hInstance);
-BOOL				InitInstance(HINSTANCE, int);
-LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE ghInst;								// current instance
 
 /* I hate C++ so bad
 const wchar_t* ProcessNameWhitelist[] = {
@@ -25,6 +17,10 @@ const wchar_t* ProcessNameWhitelist[] = {
 	NULL
 };
 */
+
+// Forward Declarations
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
 
 bool IsProcessNameInWhitelist(const wchar_t* ProcessName)
 {
@@ -39,30 +35,18 @@ bool IsProcessNameInWhitelist(const wchar_t* ProcessName)
 	return false;
 }
 
-int APIENTRY _tWinMain(HINSTANCE hInstance,
-                     HINSTANCE hPrevInstance,
-                     LPTSTR    lpCmdLine,
-                     int       nCmdShow)
+void ExecuteZap(void)
 {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-
- 	// TODO: Place code here.
-	MSG msg;
-	HACCEL hAccelTable;
-
-	// XXX: Test code here!
-
 	// Grab the current user's SID
 	PSID thisSid;
 	if (GetSecurityInfo(GetCurrentProcess(), SE_KERNEL_OBJECT, OWNER_SECURITY_INFORMATION, &thisSid, NULL, NULL, NULL, NULL) != ERROR_SUCCESS)
-		return -1;
+		return;
 
 	PWTS_PROCESS_INFO proc_info = NULL;
 	DWORD count = 0;
 	if (!WTSEnumerateProcesses(WTS_CURRENT_SERVER_HANDLE, 0, 1, &proc_info, &count)) {
 		DWORD gle = GetLastError();
-		return -1;
+		return;
 	}
 
 	for(int i=0; i < count; i++) {
@@ -75,102 +59,56 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 	}
 
 	WTSFreeMemory(proc_info);
-	//ExitWindowsEx(EWX_LOGOFF | EWX_FORCE, 
-	//			  SHTDN_REASON_MAJOR_APPLICATION | SHTDN_REASON_MINOR_INSTALLATION | SHTDN_REASON_FLAG_PLANNED);
+	ExitWindowsEx(EWX_LOGOFF | EWX_FORCE, 
+				  SHTDN_REASON_MAJOR_APPLICATION | SHTDN_REASON_MINOR_INSTALLATION | SHTDN_REASON_FLAG_PLANNED);
+}
 
-	return 0;
-
-	// Initialize global strings
-	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-	LoadString(hInstance, IDC_ZAPPED, szWindowClass, MAX_LOADSTRING);
-	MyRegisterClass(hInstance);
-
-	// Perform application initialization:
-	if (!InitInstance (hInstance, nCmdShow))
-	{
-		return FALSE;
+static bool gfHasBeenRegistered = false;
+HWND CreateScratchWindow(HWND hwndParent, WNDPROC wp)
+{
+	if (!gfHasBeenRegistered) {
+		WNDCLASS wc = {
+			0, DefWindowProc, 0, 0, ghInst, NULL,
+			LoadCursor(NULL, IDC_ARROW),
+			(HBRUSH)(COLOR_BTNFACE+1),
+			NULL, TEXT("ScratchWindow"),
+		};
+		RegisterClass(&wc);
+		gfHasBeenRegistered = true;
 	}
 
-	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_ZAPPED));
+	HWND ret;
+	ret = CreateWindow(TEXT("ScratchWindow"), NULL,
+			(hwndParent ? WS_CHILD : WS_OVERLAPPED),
+			0, 0, 0, 0, hwndParent, NULL, NULL, NULL);
+	return ret;
+}
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	MSG msg;
+	ghInst = hInstance;
+
+	// Create a hidden window that can retrieve our notifications
+	HWND hwnd = CreateScratchWindow(NULL, WndProc);
+	
+	// Register our global hotkey
+	RegisterHotKey(hwnd, 1, MOD_CONTROL | MOD_SHIFT, VK_BACK);
 
 	// Main message loop:
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
 	}
 
 	return (int) msg.wParam;
 }
 
-
-
-//
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
-//  COMMENTS:
-//
-//    This function and its usage are only necessary if you want this code
-//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
-//    function that was added to Windows 95. It is important to call this function
-//    so that the application will get 'well formed' small icons associated
-//    with it.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-	WNDCLASSEX wcex;
-
-	wcex.cbSize = sizeof(WNDCLASSEX);
-
-	wcex.style			= CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc	= WndProc;
-	wcex.cbClsExtra		= 0;
-	wcex.cbWndExtra		= 0;
-	wcex.hInstance		= hInstance;
-	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ZAPPED));
-	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_ZAPPED);
-	wcex.lpszClassName	= szWindowClass;
-	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-	return RegisterClassEx(&wcex);
-}
-
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   HWND hWnd;
-
-   hInst = hInstance; // Store instance handle in our global variable
-
-   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
 
 //
 //  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
@@ -197,7 +135,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		switch (wmId)
 		{
 		case IDM_ABOUT:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			DialogBox(ghInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
 			break;
 		case IDM_EXIT:
 			DestroyWindow(hWnd);
@@ -213,6 +151,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
+		break;
+	case WM_HOTKEY:
+		ExecuteZap();
 		break;
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
